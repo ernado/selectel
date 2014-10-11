a45334030b67b0ee6d545ec206a2d12369f585f9
ernado <ernado@ya.ru>
2014-10-11 22:28:01 +0400

    updated

--- storage/selctl/main.go
+++ storage/selctl/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/ernado/selectel/storage"
 	"github.com/jwaldrip/odin/cli"
+	"github.com/olekukonko/tablewriter"
+	"io"
 	"log"
 	"os"
 )
@@ -16,11 +18,12 @@ const (
 )
 
 var (
-	client    = cli.New("0.1", "Selectel storage command line client", connect)
-	user, key string
-	container string
-	api       storage.API
-	debug     bool
+	client         = cli.New("0.1", "Selectel storage command line client", connect)
+	user, key      string
+	container      string
+	api            storage.API
+	debug          bool
+	errorNotEnough = errors.New("Not enought arguments")
 )
 
 func init() {
@@ -28,8 +31,13 @@ func init() {
 	client.DefineStringFlag("key", "", fmt.Sprintf("selectel storage key (%s)", envKey))
 	client.DefineStringFlag("user", "", fmt.Sprintf("selectel storage user (%s)", envUser))
 	client.DefineStringFlag("container", "", fmt.Sprintf("default container (%s)", envContainer))
-	infoCommand := client.DefineSubCommand("info", "print information about storage/container/object", info)
+	infoCommand := client.DefineSubCommand("info", "print information about storage/container/object", wrap(info))
 	infoCommand.DefineStringFlag("type", "storage", "storage, container or object")
+	client.DefineSubCommand("upload", "upload object to container", wrap(upload))
+	listCommand := client.DefineSubCommand("list", "list objects in container/storage", wrap(list))
+	listCommand.DefineStringFlag("type", "storage", "storage or container")
+	downloadCommand := client.DefineSubCommand("download", "download object from container", wrap(download))
+	downloadCommand.DefineStringFlag("path", "", "destination path")
 }
 
 func readFlag(c cli.Command, name, env string) string {
@@ -63,9 +71,15 @@ func connect(c cli.Command) {
 	api.Debug(debug)
 }
 
+func wrap(callback func(cli.Command)) func(cli.Command) {
+	return func(c cli.Command) {
+		connect(c.Parent())
+		callback(c)
+	}
+}
+
 // info prints information about storage
 func info(c cli.Command) {
-	connect(c.Parent())
 	var (
 		containerName = container
 		objectName    string
@@ -117,7 +131,97 @@ func info(c cli.Command) {
 	if command == "storage" {
 		return
 	}
-	err = errors.New("Not enough arguments")
+	err = errorNotEnough
+}
+
+func upload(c cli.Command) {
+	var path string
+	switch len(c.Args()) {
+	case 1:
+		path = c.Arg(0).String()
+	case 2:
+		container = c.Arg(0).String()
+		path = c.Arg(1).String()
+	}
+	if blank(container) || blank(path) {
+		log.Fatal(errorNotEnough)
+	}
+	if err := api.Container(container).UploadFile(path); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("uploaded to %s\n", container)
+}
+
+func list(c cli.Command) {
+	var (
+		arglen = len(c.Args())
+		table  = tablewriter.NewWriter(os.Stdout)
+	)
+	if arglen == 0 && (blank(container) || c.Flag("type").String() == "storage") {
+		containers, err := api.ContainersInfo()
+		if err != nil {
+			log.Fatal(err)
+		}
+		table.SetHeader([]string{"Name", "Objects", "Type"})
+		for _, cont := range containers {
+			v := []string{cont.Name, fmt.Sprint(cont.ObjectCount), cont.Type}
+			table.Append(v)
+		}
+		table.Render()
+		return
+	}
+	if arglen == 1 {
+		container = c.Arg(0).String()
+	}
+	if blank(container) {
+		log.Fatal(errorNotEnough)
+	}
+	objects, err := api.Container(container).ObjectsInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	table.SetHeader([]string{"Name", "Size", "Downloaded"})
+	for _, object := range objects {
+		v := []string{object.Name, fmt.Sprint(object.Size), fmt.Sprint(object.Downloaded)}
+		table.Append(v)
+	}
+	table.Render()
+}
+
+func download(c cli.Command) {
+	var (
+		arglen     = len(c.Args())
+		objectName string
+		path       = c.Flag("path").String()
+	)
+	switch arglen {
+	case 1:
+		objectName = c.Arg(0).String()
+	case 2:
+		objectName = c.Arg(1).String()
+		container = c.Arg(0).String()
+	}
+	if blank(container) || blank(objectName) {
+		log.Fatal(errorNotEnough)
+	}
+	if blank(path) {
+		path = objectName
+	}
+	reader, err := api.Container(container).Object(objectName).GetReader()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer reader.Close()
+	fmt.Printf("downloading %s->%s from %s\n", objectName, path, container)
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	n, err := io.Copy(f, reader)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("downloaded %s, %d bytes\n", objectName, n)
 }
 
 func main() {
