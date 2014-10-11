fdc068b58d0e4fb3a5969902bf6ca6775bc94317
ernado <ernado@ya.ru>
2014-10-12 03:28:16 +0400

    fixed flag description

--- storage/selctl/main.go
+++ storage/selctl/main.go
@@ -83,7 +83,7 @@ func decrypt(data []byte) ([]byte, error) {
 func init() {
 	client.DefineBoolFlagVar(&debug, "debug", false, "debug mode")
 	client.DefineBoolFlagVar(&cache, "cache", false, fmt.Sprintf("cache credentials in file (%s)", envCache))
-	client.DefineBoolFlagVar(&cacheSecure, "cache.secure", true, "encrypt/decrypt token with user-key pair")
+	client.DefineBoolFlagVar(&cacheSecure, "cache.secure", true, "encrypt/decrypt token with user-key pair (true by default)")
 	client.DefineStringFlag("key", "", fmt.Sprintf("selectel storage key (%s)", envKey))
 	client.AliasFlag('k', "key")
 	client.DefineStringFlag("user", "", fmt.Sprintf("selectel storage user (%s)", envUser))
